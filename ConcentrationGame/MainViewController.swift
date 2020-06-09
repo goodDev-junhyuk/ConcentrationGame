8d0e31239735377ccff74855486cf8746d7d44c7
junhyuk <dejavukk1@gmail.com>
2020-06-09 23:20:10 +0900

    Reboot.

---
+++ ConcentrationGame/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  ConcentrationGame
+//
+//  Created by junhyuk on 2020/06/09.
+//  Copyright © 2020 junhyuk. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
