bd487145410312faf5d7d6445380b1ff5b05641f
junhyuk <dejavukk1@gmail.com>
2020-06-10 00:04:15 +0900

    카드 뒤집는 메소드 구현.

--- ConcentrationGame/Controllers/MainViewController.swift
+++ ConcentrationGame/Controllers/MainViewController.swift
@@ -19,8 +19,26 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func touchCard(_ sender: UIButton) {
-        
+        flipCard(withEmoji: "👻", on: sender)
         print("카드를 탭하였다.")
+        
+    }
+    
+    // 메소드 구현.
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        
+        if button.currentTitle == emoji {
+            
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        
+        } else {
+            
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        
+        
         
     }
     
